a763d32726a485023ec39cab9677388038c8e728
locshade <lzf01@mail.ustc.edu.cn>
2015-10-19 09:07:35 +0800

    add two files of simply get some webpage

---
+++ createUrl.py
@@ -0,0 +1,51 @@
+#-*-coding=utf-8-*-
+import urllib.request
+import io
+import gzip
+import re
+
+'''
+这个程序是使用python3语法格式进行编写的，程序的功能是首先根据初始URL获得网页
+然后将其中的链接过滤出来后存放到url.txt这个文件中然后这个文件。
+
+与此程序配套的程序是getUrlByPage.py这个文件，这个文件是将url.txt这个文件中的所有
+超链接下载并保存。
+
+'''
+
+
+'''
+函数接受一个URL链接然后返回一个网页（没有经过解码等处理）
+'''
+def url_open(url):
+    req = urllib.request.Request(url)
+    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36')
+    req.add_header('Accept-encoding', 'gzip')
+    response = urllib.request.urlopen(req)
+    html = response.read()
+    return html
+
+	
+'''
+函数接受一个URL链接然后将解码后的网页返回。
+'''
+def get_page(url):
+    bs=url_open(url)
+    bi = io.BytesIO(bs)
+    gf = gzip.GzipFile(fileobj=bi, mode="rb")
+    '''page=gf.read()
+    charS=page.info().getparam('charset')
+    return page.decode(charS,'ignore').encode('utf-8')
+    '''
+    return gf.read().decode('gbk','ignore')
+
+
+
+page=get_page('http://www.qq.com')
+p=r'href="(http:[^<>]+?\.htm)"'
+result=re.findall(p,page)
+
+
+file=open('url.txt','w')
+for each in result:
+    file.write(each+"\n")
---
+++ getPageByUrl.py
@@ -0,0 +1,56 @@
+#-*-coding=utf-8-*-
+import urllib.request
+import io
+import gzip
+
+i=1
+
+
+'''
+函数接受一个URL链接然后返回一个未经过任何处理的网页
+'''
+def url_open(url):
+    req = urllib.request.Request(url)
+    req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36')
+    req.add_header('Accept-encoding', 'gzip')
+    response = urllib.request.urlopen(req)
+    html = response.read()
+    return html
+'''
+函数接受一个URL链接然后返回一个经过解码处理之后的网页
+'''
+def get_page(url):
+    bs=url_open(url)
+    bi = io.BytesIO(bs)
+    gf = gzip.GzipFile(fileobj=bi, mode="rb")
+    '''page=gf.read()
+    charS=page.info().getparam('charset')
+    return page.decode(charS,'ignore').encode('utf-8')
+    '''
+    return gf.read().decode('gbk','ignore')
+'''
+函数接受一个网页参数然后将网页保存到文件中，文件名以此为1.html 2.html ..... 
+'''
+def save_file(html):
+    global i
+    file=open(str(i)+'.html',"w")
+    i+=1
+    file.write(html)
+    file.close()
+
+#html=get_page('http://www.baidu.com')
+
+
+file=open('url.txt')
+url=file.readline()
+while(''!=url):
+    html=get_page(url)
+    name=url.split('.')[1]
+    save_file(html,name+'.html')
+    url=file.readline()
+    print(name)
+    print(url)
+
+
+
+
